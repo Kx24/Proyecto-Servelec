a230addafb0a9e9b088702c4fc9cb4a771d96938
Kx24 <fsan.guerrero@gmail.com>
2025-10-03 00:04:48 -0300

    Cambio de Raíz Local entorno limpio, cambio de entorno tailwindcss de cli a postcss/cli

--- build-tailwind.js
+++
@@ -1,9 +0,0 @@
-const exec = require('child_process').exec;
-
-exec('./node_modules/.bin/tailwindcss -i ./Landingpage/static/landing/css/input.css -o ./Landingpage/static/landing/css/style.css --content ./Landingpage/templates/**/*.html --minify', (err, stdout, stderr) => {
-  if (err) {
-    console.error(`Error: ${stderr}`);
-    process.exit(1);
-  }
-  console.log(stdout);
-});
